cc76832859740f5cefde57524da081a1c3f7fa50
urmilparikh95 <urm777777@gmail.com>
2015-10-31 20:46:29 +0530

    seventh

--- db/hack.sql
+++ db/hack.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2015 at 11:28 AM
+-- Generation Time: Oct 31, 2015 at 04:15 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -59,8 +59,16 @@ CREATE TABLE IF NOT EXISTS `images` (
   `imgpath` varchar(500) NOT NULL,
   `img_title` varchar(30) NOT NULL,
   `img_desc` varchar(500) NOT NULL,
+  `img_rate` int(10) NOT NULL,
   PRIMARY KEY (`img_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`img_id`, `img_userid`, `imgpath`, `img_title`, `img_desc`, `img_rate`) VALUES
+(1, 3, 'full.jpg', 'abc', 'awcgaercg', 0);
 
 -- --------------------------------------------------------
 
@@ -74,17 +82,19 @@ CREATE TABLE IF NOT EXISTS `login` (
   `user_name` varchar(80) NOT NULL,
   `password` varchar(80) NOT NULL,
   `description` text NOT NULL,
-  `contact_no` bigint(10) NOT NULL,
+  `contact_no` int(10) NOT NULL,
   `impath` varchar(80) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `login`
 --
 
 INSERT INTO `login` (`ID`, `user_id`, `user_name`, `password`, `description`, `contact_no`, `impath`) VALUES
-(1, 'raju1', 'Rajesh Parikh', 'raju1', 'A craftsman with amazing skill of pottery.', 9998887771, 'C:/xampp/htdocs/SavvyKalakar/images/pot1.jpg');
+(1, 'raju1', 'Rajesh Parikh', 'raju1', 'A craftsman with amazing skill of pottery.', 2147483647, 'C:/xampp/htdocs/SavvyKalakar/images/pot1.jpg'),
+(2, 'yash1', 'yash parikh', '123', 'aevrgvaergac', 981252353, 'naman.jpg'),
+(3, 'yash2', 'yash parikh', '1234', 'ea raeths rtj ', 345324474, 'images.jpg');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
