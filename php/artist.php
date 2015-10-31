dceff1b7a423b47450087634ce5267ff94d73ad2
yash111381 <yash9998769895@gmail.com>
2015-10-31 06:25:52 -0700

    hhh

--- php/artist.php
+++ php/artist.php
@@ -9,7 +9,7 @@ $hostname = 'localhost';
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_assoc($result);
   
-	?>
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -17,32 +17,65 @@ $hostname = 'localhost';
 <title>Savvy Kalakar</title>
 	<link rel="stylesheet" type="text/css" href="../css/style1.css"/>
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>
-    
-    
 </head>
 
 <body>
-<form action="artist.php" method="post" enctype="multipart/form-data">
+
 	<script type="text/javascript" src="../js/jquery-2.1.4.min.js"></script>
-	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-    
-    
-     
-    
-    
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>  
     <div class="container" style="background-color:#000;padding-bottom:10px;border:3px dotted #FFFFFF;">
-        <div class="row" style="margin-top:10px;">
+  	 <div class="row" style="margin-top:10px;">
+   	<form action="artist.php" method="post" enctype="multipart/form-data">    
 		<input type="submit" name="commit1" value="Logout" class="btn btn-danger col-md-1 col-md-offset-10" />
-        </div>
+     </form>
+         </div>
     </div>
-	<h2 class = "col-md-4 col-md-offset-8">Welcome <?php echo $row['user_name']; ?></h2>
-<?php 
+    <h2 class = "col-md-4 col-md-offset-8">Welcome <?php echo $row['user_name']; ?></h2>
+    <?php 
 if(isset($_POST['commit1'])){
 	session_unset();
 	session_destroy();
 	echo "<script>window.location.href='../index.php'</script>";
 }
-	?>
-    </form>
+$uname=$row["user_name"];
+$ucontact=$row["contact_no"];
+$desc=$row["description"];
+$imgpath=$row["impath"];
+
+?>
+	
+
+.
+<div class="media">
+        <div class="media-left" >
+            <a href="#">
+            
+                <img style="margin-left:20px;float:left;" class="media-object" src="<?php echo $imgpath; ?>" width="350" height="300">
+               
+            </a>
+             
+        </div>
+        <div class="media-body">
+            <h4 class="media-heading">User NAME</h4>
+             <?php echo $uname; ?>
+        </div>
+        
+        <div class="media-body">
+            <h4 class="media-heading">User Contact No.</h4>
+             <?php echo $ucontact; ?>
+        </div>
+       
+        
+        <div class="media-body">
+            <h4 class="media-heading">User Description</h4>
+             <?php echo $desc; ?>
+        </div>
+      
+ </div>
+
+
+
+
+   
 </body>
 </html>
